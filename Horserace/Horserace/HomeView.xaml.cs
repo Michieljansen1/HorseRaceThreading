d03f39bf19a65f07cf38d6f54481589d0881de6f
Yaron <yaronlambers@gmail.com>
2020-03-31 11:00:41 +0200

    Changed Mainview to homeview

--- Horserace/Horserace/HomeView.xaml.cs
+++ Horserace/Horserace/HomeView.xaml.cs
@@ -22,9 +22,9 @@ namespace Horserace
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class HomeView : Page
     {
-        public MainPage()
+        public HomeView()
         {
             this.InitializeComponent();
 
