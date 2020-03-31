d4d8c5a16909f7b3d7cb938a028ce2f971f6103b
Thomas Christerus <tchristerus@outlook.com>
2020-03-31 13:05:43 +0200

    Add: ping using threadpool

--- Horserace/Horserace/Common/Ping.cs
+++ Horserace/Horserace/Common/Ping.cs
@@ -1,30 +1,47 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
+using Windows.AI.MachineLearning;
+using Windows.Foundation;
 using Windows.Networking;
 using Windows.Networking.Sockets;
+using Windows.UI.Xaml;
 
 namespace Horserace.Common
 {
-    class Ping 
+    class Ping
     {
-        public Ping(string url)
-        {
-            
-        }
 
-        public async void StartPing(string url)
+        int _totalPings;
+        private string _url;
+        private int _previousPingTime;
+        private int _totalTime;
+
+        public Ping(string url, int totalPings)
         {
+            _url = url;
+            _totalPings = totalPings;
+        }
         
-            Stopwatch stopwatch = new Stopwatch();
-        
-        
-            stopwatch.Start();
-            StreamSocket socket = new StreamSocket();
-            await socket.ConnectAsync(new HostName(url), "80");
-            stopwatch.Stop();
-        
-            Debug.WriteLine($"url {url} time: {stopwatch.ElapsedMilliseconds}");
-        
+
+        public void StartPing()
+        {
+
+            IAsyncAction pingAction = Windows.System.Threading.ThreadPool.RunAsync(async (workItem) =>
+            {
+                int i = 0;
+                while (i < _totalPings)
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    StreamSocket socket = new StreamSocket();
+                    stopwatch.Start();
+                    await socket.ConnectAsync(new HostName(_url), "80");
+                    stopwatch.Stop();
+                    Debug.WriteLine($"url {_url} time: {stopwatch.ElapsedMilliseconds} systemTime: {System.DateTime.Now}");
+                    Thread.Sleep(1000);
+                    i++;
+                }
+            });
         }
     }
 }
