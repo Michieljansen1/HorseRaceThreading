89f0c4a3dda2d6307c5cd371fcd18271ed984bff
Thomas Christerus <tchristerus@outlook.com>
2020-04-02 12:09:39 +0200

    Fix: Reformat PageLoader

--- Horserace/Horserace/Common/PageLoader.cs
+++ Horserace/Horserace/Common/PageLoader.cs
@@ -1,32 +1,28 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Windows.Web.Http;
 
 namespace Horserace.Common
 {
     /// <summary>
-    /// Determine the size of a webpage class
+    ///     Determine the size of a webpage class
     /// </summary>
     class PageLoader
     {
+        /// <summary>
+        ///     list of pages to check the content for
+        /// </summary>
+        private readonly string[] _pages = { "/", "/robots.txt", "/contact", "/sitemap.xml" };
+
         private int _totalSize; // Total size of all pages
 
         /// <summary>
-        /// list of pages to check the content for
-        /// </summary>
-        private readonly string[] _pages = {
-            "/",
-            "/robots.txt",
-            "/contact",
-            "/sitemap.xml",
-        };
-
-        /// <summary>
-        /// Fetches the content of the give URL webpage and returns the DOM size
+        ///     Fetches the content of the give URL webpage and returns the DOM size
         /// </summary>
         /// <param name="url">URL to fetch</param>
         private async Task<int> GetDomSize(string url)
         {
-            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            HttpClient httpClient = new HttpClient();
 
             var headers = httpClient.DefaultRequestHeaders;
 
@@ -41,18 +37,17 @@ namespace Horserace.Common
             {
                 throw new Exception("Invalid header value: " + header);
             }
-            
+
             var requestUri = new Uri(url);
 
             //Send the GET request asynchronously and retrieve the response as a string.
-            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
 
             try
             {
                 httpResponse = await httpClient.GetAsync(requestUri);
                 return httpResponse.Content.ToString().Length;
-            }
-            catch (Exception ex)
+            } catch (Exception ex)
             {
                 return 0;
             }
@@ -60,14 +55,14 @@ namespace Horserace.Common
 
 
         /// <summary>
-        ///  the size of the total site
+        ///     the size of the total site
         /// </summary>
         /// <param name="url">Base url to fetch</param>
         public async Task<int> Run(string url)
         {
             foreach (var page in _pages)
             {
-                _totalSize += await GetDomSize("https://"+url+page);
+                _totalSize += await GetDomSize("https://" + url + page);
             }
 
             return _totalSize / 10;
