d566806c8ee2e3f57aab85d14244e1c7183a5b86
Thomas Christerus <tchristerus@outlook.com>
2020-04-02 19:24:01 +0200

    Fix: refactor ToastUtil

--- Horserace/Horserace/Utils/ToastUtil.cs
+++ Horserace/Horserace/Utils/ToastUtil.cs
@@ -1,53 +1,36 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
-using System;
+﻿using System;
 using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Horserace.Utlis
 {
+    /// <summary>
+    ///     Manages the toast to inform the user
+    /// </summary>
     class ToastUtil
     {
         /// <summary>
-        /// Builds and shows a toast
+        ///     Builds and shows a toast
         /// </summary>
         /// <param name="title">Title that appears on the toast</param>
         /// <param name="content">Text inside the toast</param>
         public static void Notify(String title, String content)
         {
-
             // Building the toast layout 
-            ToastVisual visual = new ToastVisual()
-            {
-                BindingGeneric = new ToastBindingGeneric()
-                {
-                    Children =
-                {
-                    new AdaptiveText()
-                    {
-                        Text = title
-                    },
-
-                    new AdaptiveText()
-                    {
-                        Text = content
-                    }
-                },
-                    AppLogoOverride = new ToastGenericAppLogo()
-                    {
-                        Source = "Assets/Square44x44Logo.scale-400.png",
-                        HintCrop = ToastGenericAppLogoCrop.Circle
+            var visual = new ToastVisual {
+                BindingGeneric = new ToastBindingGeneric {
+                    Children = {new AdaptiveText {Text = title}, new AdaptiveText {Text = content}},
+                    AppLogoOverride = new ToastGenericAppLogo {
+                        Source = "Assets/Square44x44Logo.scale-400.png", HintCrop = ToastGenericAppLogoCrop.Circle
                     }
                 }
             };
 
             // Adding toast content
-            ToastContent toastContent = new ToastContent()
-            {
-                Visual = visual
-            };
+            var toastContent = new ToastContent {Visual = visual};
 
             // Sending and displaying the notification
-            var toast = new ToastNotification(toastContent.GetXml());
-            toast.Priority = ToastNotificationPriority.High;
+            var toast = new ToastNotification(toastContent.GetXml()) {Priority = ToastNotificationPriority.High};
             ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
     }
